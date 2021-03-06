afbcb2e89ec4156aef39eca38a0f577613d3ef89
easierbycode <daniel@easierbycode.com>
2021-03-06 16:38:39 -0700

    validate with decorators

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -22,6 +33,63 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __param = (this && this.__param) || function (paramIndex, decorator) {
     return function (target, key) { decorator(target, key, paramIndex); }
 };
+var registeredValidators = {};
+function Required(target, propName) {
+    var _a;
+    registeredValidators[target.constructor.name] = __assign(__assign({}, registeredValidators[target.constructor.name]), (_a = {}, _a[propName] = ['required'], _a));
+}
+function PositiveNumber(target, propName) {
+    var _a;
+    registeredValidators[target.constructor.name] = __assign(__assign({}, registeredValidators[target.constructor.name]), (_a = {}, _a[propName] = ['positive'], _a));
+}
+function Validate(obj) {
+    var objValidatorConfig = registeredValidators[obj.constructor.name];
+    if (!objValidatorConfig) {
+        return true;
+    }
+    var isValid = true;
+    for (var prop in objValidatorConfig) {
+        for (var _i = 0, _a = objValidatorConfig[prop]; _i < _a.length; _i++) {
+            var validator = _a[_i];
+            switch (validator) {
+                case 'required':
+                    isValid = isValid && !!obj[prop];
+                    break;
+                case 'positive':
+                    isValid = isValid && obj[prop] > 0;
+                    break;
+            }
+        }
+    }
+    return isValid;
+}
+var Course = /** @class */ (function () {
+    function Course(t, p) {
+        this.title = t;
+        this.price = p;
+    }
+    __decorate([
+        Required
+    ], Course.prototype, "title", void 0);
+    __decorate([
+        PositiveNumber
+    ], Course.prototype, "price", void 0);
+    return Course;
+}());
+var courseForm = document.querySelector('form');
+courseForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var titleEl = document.getElementById('title');
+    var priceEl = document.getElementById('price');
+    var title = titleEl.value;
+    var price = +priceEl.value;
+    var createdCourse = new Course(title, price);
+    if (!Validate(createdCourse)) {
+        alert('Invalid input 👎');
+        return;
+    }
+    console.log(createdCourse);
+});
 function Autobind(target, methodName, descriptor) {
     var originalMethod = descriptor.value;
     // modified propertyDescriptor
--- app.ts
+++ app.ts
@@ -1,3 +1,80 @@
+interface ValidatorConfig {
+  [property: string]: {
+    [validateableProp: string]: string[]  // ['required', 'positive']
+  }
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['required']
+  };
+}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: ['positive']
+  };
+}
+
+function Validate(obj: any) {
+  const objValidatorConfig = registeredValidators[obj.constructor.name];
+  if (!objValidatorConfig) {
+    return true;
+  }
+  let isValid = true;
+  for (const prop in objValidatorConfig) {
+    for (const validator of objValidatorConfig[prop]) {
+      switch (validator) {
+        case 'required':
+          isValid = isValid && !!obj[prop];
+          break;
+        case 'positive':
+          isValid = isValid && obj[prop] > 0;
+          break;
+      }
+    }
+  }
+
+  return isValid;
+}
+
+class Course {
+  @Required
+  title: string;
+  @PositiveNumber
+  price: number;
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this.price = p;
+  }
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const titleEl = document.getElementById('title') as HTMLInputElement;
+  const priceEl = document.getElementById('price') as HTMLInputElement;
+
+  const title = titleEl.value;
+  const price = +priceEl.value;
+
+  const createdCourse = new Course(title, price);
+
+  if (!Validate(createdCourse)) {
+    alert('Invalid input 👎');
+    return;
+  }
+  console.log(createdCourse);
+});
+
+
+
+
 function Autobind(target: any, methodName: string | Symbol, descriptor: PropertyDescriptor) {
   const originalMethod = descriptor.value;
   // modified propertyDescriptor
