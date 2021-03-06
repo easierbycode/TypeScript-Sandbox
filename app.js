6eb5397866a91a8ed97446b9c7a2fc1136177de3
easierbycode <daniel@easierbycode.com>
2021-03-06 13:36:18 -0700

    use decorator factory

--- app.js
+++ app.js
@@ -4,17 +4,24 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-// Logger Fn will receive Person class constructor as arg
-function Logger(ctxFn) {
-    console.log('!!! Logging !!!');
+// Decorator factory
+// - the fn it returns is decorator
+// - this approach allows outer factory function to accept params
+//   which can be used by inner decorator function
+function Logger(logMessage) {
+    // Logger Fn will receive Person class constructor as arg
+    return function (ctxFn) {
+        console.log(logMessage);
+        console.log('!!! Logging !!!');
+    };
 }
 var Person = /** @class */ (function () {
     function Person() {
         this.name = 'Max';
-        console.log('creating person...');
+        console.log('creating person ...');
     }
     Person = __decorate([
-        Logger
+        Logger('[LOGGING]  Person')
     ], Person);
     return Person;
 }());
--- app.ts
+++ app.ts
@@ -1,17 +1,24 @@
 
-// Logger Fn will receive Person class constructor as arg
-function Logger(ctxFn: Function) {
+// Decorator factory
+// - the fn it returns is decorator
+// - this approach allows outer factory function to accept params
+//   which can be used by inner decorator function
+function Logger(logMessage: string) {
+  // Logger Fn will receive Person class constructor as arg
+  return function(ctxFn: Function) {
+    console.log(logMessage);
     console.log('!!! Logging !!!');
   }
+}
   
-  @Logger
-  class Person {
-    name = 'Max';
-    
-    constructor() {
-      console.log('creating person...')
-    }
+@Logger('[LOGGING]  Person')
+class Person {
+  name = 'Max';
+  
+  constructor() {
+    console.log('creating person ...')
   }
+}
 
 
-  const pers = new Person();
+const pers = new Person();
